9fcc77213e4d19fadd32b929573eabb84c22c1bf
gautam <tpython719@gmail.com>
2024-10-22 12:04:05 +0530

    add/subtract/remove quantity from cart/pricing/tax calculations

--- carts/urls.py
+++ carts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('cart/', cart_page, name='cart_page'),
     path('cart/<int:product_id>/', add_cart, name='add_to_card_page'),
     path('cart_remove/<int:product_id>/', remove_cart, name='remove_to_card_page'),
+    path('cart_item_remove/<int:product_id>/', remove_cart_item, name='remove_item_from_cart'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import CartItem, Cart
 from django.core.exceptions import ObjectDoesNotExist
@@ -35,27 +35,24 @@ def add_cart(request, product_id):
     return redirect('cart_page')
 
 def remove_cart(request, product_id):
-    product = Product.objects.get(id=product_id) # Get the product for Cart
-    try:
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-    except Cart.DoesNotExist:
-        cart = Cart.objects.create(
-            cart_id=_cart_id(request)
-        )
-        cart.save()
-
-    try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)  # Get the product for Cart
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
         cart_item.quantity -= 1
         cart_item.save()
-    except CartItem.DoesNotExist:
-        cart_item = CartItem.objects.create(
-            product = product,
-            cart = cart,
-            quantity = 1
-        )
-        cart_item.save()
+    else:
+        cart_item.delete()
     return redirect('cart_page')
+
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)  # Get the product for Cart
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart_page')
+
 
 def cart_page(request, total_price=0, quantity=0, cart_items=None):
     try:
@@ -64,6 +61,9 @@ def cart_page(request, total_price=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total_price += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (18 * total_price) / 100
+        grand_total = total_price+tax
+        length = len(cart_items)
     except ObjectDoesNotExist:
         pass
 
@@ -71,6 +71,9 @@ def cart_page(request, total_price=0, quantity=0, cart_items=None):
         'total': total_price,
         'quantity' : quantity,
         'cart_items': cart_items,
+        'tax': round(tax),
+        'grand_total': round(grand_total),
+        'length': length,
     }
 
     return render(request, 'store/cart.html', context)
